f328bc546f759c5e23f48fad8d55133d37650840
Carolina Bonitatis <carolina@bonitat.is>
2023-10-03 16:21:14 -0400

    work on Column test

--- client/components/dashboard/Column.test.jsx
+++ client/components/dashboard/Column.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Dashboard from './Dashboard.jsx';
-import { render, screen } from '@testing-library/react'; //screen allows to select element from the DOM
+import { render, screen, fireEvent } from '@testing-library/react'; //screen allows to select element from the DOM
 import { expect, beforeEach, it } from 'vitest';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -12,15 +12,29 @@ beforeEach(() => {
     );
   });
 
-//we have 2 buttons create and task small
-
-
+// test if create button exists
 it('renders a CREATE button'), () =>{
-    const createButton = screen.getByRole('button');
-    expect(createButton).toBeInTheDocument();
+  const createButton = screen.getByRole('button');
+  expect(createButton).toBeInTheDocument();
 }
-//create button should create a small task card
-it('create button should create a small task card'), () =>{
-    const taskSmall = screen.getByRole('button');
-    expect(taskSmall).toBeInTheDocument();
-}
+
+// test to check if create button pops-up when click. 
+// TODO: generate smallTaskCard to be tested
+it('clicking the button opens a pop-up window', () => {
+  // Render your component
+  render(<Dashboard />);
+
+  // Find the button by its ARIA role
+  const button = screen.getByRole('button');
+
+  // Check that the pop-up window is not initially visible
+  const popupCard = screen.queryByText('Popup card'); // Replace with your popup content text or element
+  expect(popupCard).not.toBeInTheDocument();
+
+  // Simulate a user click on the button
+  fireEvent.click(button);
+
+  // Check that the pop-up window is now visible
+  const newPopupCard = screen.getByText('Popup card'); // Replace with your popup content text or element
+  expect(newPopupWindow).toBeInTheDocument();
+});
