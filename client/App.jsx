337bd8f2169dbf6a952a66a58e0d6aeb1293ba76
Gabriela Passentino <127862221+gpassentino@users.noreply.github.com>
2023-10-03 16:45:13 -0500

    Gabby/login (#5)
    * commiting login
    
    * finished login but need to update URL
    
    * going to merge with dev
    
    * continuing on dashboard and topnavbar

---
+++ client/App.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Login } from './components/auth/Login.jsx';
+import { SignUp } from './components/auth/SignUp.jsx';
+import { Dashboard } from './components/dashboard/Dashboard.jsx';
+
+const App = () => {
+  const router = createBrowserRouter([
+    { path: '/', element: <Login /> },
+    { path: '/signup', element: <SignUp /> },
+    { path: '/dashboard', element: <Dashboard /> },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default App;
