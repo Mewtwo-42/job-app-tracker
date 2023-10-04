a523513b33c7ab5d2957f174f4eff6b74dcefe6e
Carolina Bonitatis <carolina@bonitat.is>
2023-10-04 13:34:58 -0400

    work in progress column component test

--- client/components/dashboard/Column.test.jsx
+++ client/components/dashboard/Column.test.jsx
@@ -1,40 +1,38 @@
 import React from 'react';
-import Dashboard from './Dashboard.jsx';
-import { render, screen, fireEvent } from '@testing-library/react'; //screen allows to select element from the DOM
-import { expect, beforeEach, it } from 'vitest';
-import { BrowserRouter } from 'react-router-dom';
+import { Column } from './Column';
+import { render } from '@testing-library/react'; //screen allows to select element from the DOM
+import { expect, it } from 'vitest';
+// import { BrowserRouter } from 'react-router-dom';
 
-beforeEach(() => {
-    render(
-      <BrowserRouter>
-        <Dashboard/>
-      </BrowserRouter>
-    );
+// test to check if Column component renders
+it ('renders the Column component', () => {
+      render(<Column/>);
   });
 
 // test if create button exists
 it('renders a CREATE button'), () =>{
-  const createButton = screen.getByRole('button');
-  expect(createButton).toBeInTheDocument();
+  const createButton = screen.getByRole('create');
+  expect(createButton).toBeVisible();
 }
 
-// test to check if create button pops-up when click. 
-// TODO: generate smallTaskCard to be tested
-it('clicking the button opens a pop-up window', () => {
-  // Render your component
-  render(<Dashboard />);
 
-  // Find the button by its ARIA role
-  const button = screen.getByRole('button');
+// // test to check if create button pops-up when click. 
+// // TODO: generate smallTaskCard to be tested
+// it('clicking the button opens a pop-up window', () => {
+//   // Render your component
+//   render(<Dashboard />);
 
-  // Check that the pop-up window is not initially visible
-  const popupCard = screen.queryByText('Popup card'); // Replace with your popup content text or element
-  expect(popupCard).not.toBeInTheDocument();
+//   // Find the button by its ARIA role
+//   const button = screen.getByRole('button');
 
-  // Simulate a user click on the button
-  fireEvent.click(button);
+//   // Check that the pop-up window is not initially visible
+//   const popupCard = screen.queryByText('Popup card'); // Replace with your popup content text or element
+//   expect(popupCard).not.toBeInTheDocument();
 
-  // Check that the pop-up window is now visible
-  const newPopupCard = screen.getByText('Popup card'); // Replace with your popup content text or element
-  expect(newPopupCard).toBeInTheDocument();
-});
+//   // Simulate a user click on the button
+//   fireEvent.click(button);
+
+//   // Check that the pop-up window is now visible
+//   const newPopupCard = screen.getByText('Popup card'); // Replace with your popup content text or element
+//   expect(newPopupCard).toBeInTheDocument();
+// });
--- server/server.test.js
+++ server/server.test.js
@@ -1,14 +1,15 @@
 const server = 'http://localhost:3000';
 import { expect, test } from 'vitest';
 import request from 'supertest';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
 //enable provess.env
 dotenv.config();
 
 // Cookie value provided in /auth testing suite and used in /logs testing suite
-let cookieValue;
+// let cookieValue;
 
+// auth testing when the post request is made with correct password
 test('/auth responds to POST request with 200 status when provided with correct password', () => {
   return request(server)
     .post('/api/auth')
@@ -16,4 +17,19 @@ test('/auth responds to POST request with 200 status when provided with correct 
       serverPassword: process.env.VITE_USER_PASSWORD,
     })
     .expect(200);
+});
+
+// auth testing when the post request is made with incorrect password
+test('/auth responds to POST request with 401 status when provided with incorrect password', () => {
+  return request(server)
+    .post('/api/auth')
+    .send({
+      serverPassword: 'wrongPassword',
+    })
+    .expect(401);
+});
+
+// /task testing when a get request is made
+test('/task responds to GET request with 200 status', () => {
+  return request(server).get('/api/task').expect(200);
 });
